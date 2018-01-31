bce14bb3ce4ce1fdccc69e117abfca88daf48165
manana <manana@Core11-15>
2018-01-31 13:53:41 +0100

    Ejercicio tienda modelo servidor -cliente con bbdd ,JSON y comms

---
+++ 09_servidor_tienda/src/comunicaciones/HiloClienteRegistro.java
@@ -0,0 +1,59 @@
+package comunicaciones;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import beans.Pedidos;
+import modelo.GestionTienda;
+
+public class HiloClienteRegistro implements Runnable {
+
+	private Socket sc;
+	List<Pedidos> pedidos=null;
+	@Override
+	public void run() {
+		try {
+			InputStream is=sc.getInputStream();
+			OutputStream os=sc.getOutputStream();
+			//envio de datos es una operacion de escritura con un printstream
+			try(PrintStream salida = new PrintStream(os);BufferedReader bf=new BufferedReader(new InputStreamReader (is));){
+				JSONParser parser=new JSONParser();
+				JSONArray array=(JSONArray) parser.parse(new InputStreamReader(is));
+				for(Object ob:array) {
+					JSONObject data=(JSONObject) ob;
+					GestionTienda tienda=new GestionTienda("",0,"",null);
+					tienda.crearPedido(data.get("producto").toString(), Integer.parseInt(data.get("unidades").toString()),
+							data.get("ipCliente").toString(), LocalDateTime.parse(data.get("fecha").toString()));
+					
+				}
+				
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}catch(IOException ex) {
+			ex.printStackTrace();
+		}finally {
+			try {
+				sc.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
---
+++ 09_servidor_tienda/src/modelo/GestionTienda.java
@@ -0,0 +1,67 @@
+package modelo;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import beans.Pedidos;
+
+public class GestionTienda extends Pedidos {
+
+	
+	public GestionTienda(String producto, int unidades, String ipcliente, LocalDateTime fecha) {
+		super(producto, unidades, ipcliente, fecha);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void crearPedido(String producto,int unidades,String ipCliente,LocalDateTime fecha) {
+		java.sql.Date fechaSql=(Date) java.sql.Date.from(fecha.toInstant(null));
+		
+		try (Connection cn=DriverManager.getConnection("jdbc:mysql://localhost:3306/tiendas", "root", "root"))
+		{
+			String sql="Insert into pedidos(producto,unidades,ipCliente,fecha) values(?,?,?,?)";
+			PreparedStatement ps = cn.prepareStatement(sql);
+			ps.setString(1,producto);
+			ps.setInt(2,unidades);
+			ps.setString(3, ipCliente);
+			ps.setDate(4, fechaSql);
+			ps.execute();
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		 
+	}
+	
+	public List<Pedidos>devolverPedido(String producto, int unidades) {
+		ArrayList<Pedidos> pedidos=new ArrayList<>();
+		Pedidos pedido=null;
+		LocalDateTime fecha=null;
+		
+		try (Connection cn=DriverManager.getConnection("jdbc:mysql://localhost:3306/tiendas", "root", "root"))
+		{
+			String sql="select producto,unidades,ipCliente,fecha from pedidos where producto=='" + producto +
+					"' and unidades=='" + unidades + "'" ;
+			Statement st = cn.createStatement();
+			ResultSet rs=st.executeQuery(sql);
+		
+			while (rs.next()) {
+				fecha=LocalDateTime.parse(rs.getDate("fecha").toString());
+				pedido = new Pedidos(rs.getString("producto"),rs.getInt("unidades"),rs.getString("ipCliente"),fecha);
+				pedidos.add(pedido);
+				
+			}
+			
+		}catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		return pedidos;
+		}
+}
